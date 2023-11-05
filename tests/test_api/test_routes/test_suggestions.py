4554b7f076b042d56b7cf40b7697562285c3bfbb
jdpro <joadorn@gmail.com>
2023-11-05 05:43:11 +0100

    Fix tests

--- tests/test_api/test_routes/test_suggestions.py
+++ tests/test_api/test_routes/test_suggestions.py
@@ -6,31 +6,31 @@ from fastapi.testclient import TestClient
 
 from app.main import app
 
-client = TestClient(app)
-
 
 def test_create_suggestion():
     """
     Test the creation of suggestions
     """
-    response = client.post("/api/v1/suggest/keywords", json={
-        "title": "Foo",
-        "description": "There goes my hero"
-    })
-    assert response.status_code == 200
-    assert "keywords" in response.json()
-    assert len(response.json()["keywords"]) == 2
-    assert "foo" in response.json()["keywords"]
+    with TestClient(app) as client:
+        response = client.post("/api/v1/suggest/keywords", json={
+            "title": "Foo",
+            "description": "There goes my hero"
+        })
+        assert response.status_code == 200
+        assert "keywords" in response.json()
+        assert len(response.json()["keywords"]) == 2
+        assert "foo" in response.json()["keywords"]
 
 
 def test_create_suggestion_with_empty_metadata():
     """
     Test sening empty metadata to API
     """
-    response = client.post("/api/v1/suggest/keywords", json={
-        "title": "",
-        "description": ""
-    })
-    assert response.status_code == 200
-    assert "keywords" in response.json()
-    assert len(response.json()["keywords"]) == 0
+    with TestClient(app) as client:
+        response = client.post("/api/v1/suggest/keywords", json={
+            "title": "",
+            "description": ""
+        })
+        assert response.status_code == 200
+        assert "keywords" in response.json()
+        assert len(response.json()["keywords"]) == 0
